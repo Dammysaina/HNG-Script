d5a4a2a0d2494b0a70026da39be3b3f12a444959
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2020-06-03 08:39:25 +0100

    added damilola file

---
+++ scripts/oluwadamilola.adediran.js
@@ -0,0 +1,5 @@
+const displayMessage = (hngID, fullName, language) => {
+    return(console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`));
+};
+
+displayMessage("Damilola Adediran", "02794",  "Javascript");
