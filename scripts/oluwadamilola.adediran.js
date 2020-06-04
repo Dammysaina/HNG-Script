d8ad6b566221b772d551a0f2f114a42def3ac87b
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2020-06-04 02:30:11 +0100

    task updated

--- scripts/oluwadamilola.adediran.js
+++ scripts/oluwadamilola.adediran.js
@@ -1,5 +1,6 @@
-const displayMessage = (hngID, fullName, language) => {
-    return(console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`));
-};
-
-displayMessage("Damilola Adediran", "02794",  "Javascript");
+function displayMessage(value) {
+    return console.log(value);
+  }
+  
+  stage2 = { fullName : "Oluwadamilola Adediran", hngID: "HNG-02794", language : "JavaScript", email:  };
+  details(`Hello world, this is ${task.fullName} with HNGi7 ID ${task.hngID} using ${task.language} for Stage 2 task`);
